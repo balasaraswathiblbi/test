a22e05d59418226f1a05e4abd69d853fe05c71e2
Bala Saraswathi <balasaraswathi@billionlives.in>
2022-05-25 10:52:46 +0530

    Project

---
+++ src/Welcome.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Test extends React.Component{
+
+  
+  
+
+  render(){
+    
+    return(
+      <div>      
+      
+        Hello world from Test
+        
+      </div>
+    )
+  }
+}
